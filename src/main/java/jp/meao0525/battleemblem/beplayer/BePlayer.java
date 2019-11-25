71a52941d32e4cf63aab69d8a770aedfa84c8ae2
meao0525 <break.grips@gmail.com>
2019-11-25 17:53:08 +0900

    バトルクラスをセットする
    BePlayerにバトルクラスをセットする処理setBattleClassを追加
    上手く書けてるといいなあ

--- src/main/java/jp/meao0525/battleemblem/beplayer/BePlayer.java
+++ src/main/java/jp/meao0525/battleemblem/beplayer/BePlayer.java
@@ -1,6 +1,7 @@
 package jp.meao0525.battleemblem.beplayer;
 
 import jp.meao0525.battleemblem.battleclass.BattleClass;
+import jp.meao0525.battleemblem.battleclass.ClassStatus;
 import org.bukkit.entity.Player;
 
 public class BePlayer {
@@ -13,10 +14,33 @@ public class BePlayer {
     }
 
     public void setBattleClass(BattleClass battleClass) {
+        //headerにクラス名をセット
         player.setPlayerListHeader(battleClass.getName());
+        //プレイヤーのステータスをセットしていくよ
+        ClassStatus status = battleClass.getStatus();
+        player.setHealthScale(status.getHp()); //HP
+        player.setWalkSpeed(status.getSpeed()); //足の速さ
+        setAttack(status.getAttack()); //攻撃
+        setDefence(status.getDefence()); //防御
     }
 
     public Player getPlayer() {
         return player;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
     }
 }
