0631dbd2d6eb9f18b6254e8ed0700cb59fef84bb
meao0525 <break.grips@gmail.com>
2019-11-25 17:15:07 +0900

    github 大丈夫？

--- src/main/java/jp/meao0525/battleemblem/beplayer/BePlayer.java
+++ src/main/java/jp/meao0525/battleemblem/beplayer/BePlayer.java
@@ -1,12 +1,19 @@
 package jp.meao0525.battleemblem.beplayer;
 
+import jp.meao0525.battleemblem.battleclass.BattleClass;
 import org.bukkit.entity.Player;
 
 public class BePlayer {
     private Player player;
+    private int attack;
+    private int defence;
 
     public BePlayer(Player player) {
         this.player = player;
+    }
+
+    public void setBattleClass(BattleClass battleClass) {
+        player.setPlayerListHeader(battleClass.getName());
     }
 
     public Player getPlayer() {
