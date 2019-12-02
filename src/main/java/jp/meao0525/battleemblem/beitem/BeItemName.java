f71da2d2022ccb4fdd596ffb7e4b2ee69181e1f5
meao0525 <break.grips@gmail.com>
2019-12-02 19:13:34 +0900

    BeItemNameの作成(まだ途中)

---
+++ src/main/java/jp/meao0525/battleemblem/beitem/BeItemName.java
@@ -0,0 +1,13 @@
+package jp.meao0525.battleemblem.beitem;
+
+public class BeItemName {
+    /*===ゲームアイテム===*/
+    public static String LOADOUT_SELECTOR_NAME = "ロードアウトセレクター";
+    public static String BATTLE_CLASS_LIST = "バトルクラス一覧";
+    /*===クラスアイテム===*/
+    public static String MASTER_SWORD_NAME = "剣聖の剣";
+    public static String BERSERKER_AXE_NAME = "狂戦士の戦斧";
+    public static String KNIGHT_AXE_NAME = "重鎧兵の戦斧";
+    public static String BRAVE_SWORD_NAME = "勇者の剣";
+    public static String DAGGER_SWORD_NAME = "暗殺者の短剣";
+}
