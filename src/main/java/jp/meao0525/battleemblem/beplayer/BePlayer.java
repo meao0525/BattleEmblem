8ad54b44f72ca22794badf17ac62fd307e6e2682
meao0525 <break.grips@gmail.com>
2019-12-04 11:53:10 +0900

    BePlayerで装備を与える処理を書いたぜ！
    ifで重鎧兵と勇者の時だけ鎧を与えているんだけどなんか気持ち悪い

--- src/main/java/jp/meao0525/battleemblem/beplayer/BePlayer.java
+++ src/main/java/jp/meao0525/battleemblem/beplayer/BePlayer.java
@@ -2,8 +2,10 @@ package jp.meao0525.battleemblem.beplayer;
 
 import jp.meao0525.battleemblem.battleclass.BattleClass;
 import jp.meao0525.battleemblem.battleclass.ClassStatus;
+import jp.meao0525.battleemblem.beitem.BeItems;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class BePlayer {
     private Player player;
@@ -19,19 +21,24 @@ public class BePlayer {
         battleClass.setUsed(true);
 
         //headerにクラス名をセット
-        player.setPlayerListHeader(battleClass.getName());
+        getPlayer().setPlayerListHeader(battleClass.getName());
 
         //プレイヤーのステータスをセットしていくよ
         ClassStatus status = battleClass.getStatus();
-        player.setHealthScale(status.getHp()); //HP
-        player.setWalkSpeed(status.getSpeed()); //足の速さ
+        getPlayer().setHealthScale(status.getHp()); //HP
+        getPlayer().setWalkSpeed(status.getSpeed()); //足の速さ
         setAttack(status.getAttack()); //攻撃
         setDefence(status.getDefence()); //防御
 
-        //TODO: 装備を与える
+        //装備を与える
+        getPlayer().getInventory().addItem(battleClass.getItem().toItemStack());
+        //鎧は必要ですか？
+        if (battleClass.equals(BattleClass.ARMOR_KNIGHT) || battleClass.equals(BattleClass.BRAVE_HERO)) {
+            setArmor(getPlayer(), battleClass);
+        }
 
         //ロードアウトセレクター回収する
-        player.getInventory().remove(Material.EMERALD);
+        getPlayer().getInventory().remove(Material.EMERALD);
     }
 
     public void removeBattleClass() {
@@ -60,5 +67,22 @@ public class BePlayer {
 
     public void setDefence(int defence) {
         this.defence = defence;
+    }
+
+    private void setArmor(Player player, BattleClass battleClass) {
+        //同じ操作してるのが気持ち悪い
+        if (battleClass.equals(BattleClass.ARMOR_KNIGHT)) {
+            //重鎧兵の装備
+            player.getInventory().setHelmet(BeItems.KNIGHT_HELMET.toItemStack());
+            player.getInventory().setChestplate(BeItems.KNIGHT_CHESTPLATE.toItemStack());
+            player.getInventory().setLeggings(BeItems.KNIGHT_LEGGINGS.toItemStack());
+            player.getInventory().setBoots(BeItems.KNIGHT_BOOTS.toItemStack());
+        } else {
+            //勇者の装備
+            player.getInventory().setHelmet(BeItems.BRAVE_HELMET.toItemStack());
+            player.getInventory().setChestplate(BeItems.BRAVE_CHESTPLATE.toItemStack());
+            player.getInventory().setLeggings(BeItems.BRAVE_LEGGINGS.toItemStack());
+            player.getInventory().setBoots(BeItems.BRAVE_BOOTS.toItemStack());
+        }
     }
 }
