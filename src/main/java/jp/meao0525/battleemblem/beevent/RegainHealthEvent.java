f61394cbf1cc42038b0dc4477ea9974e5e07ce7a
meao0525 <50980996+meao0525@users.noreply.github.com>
2020-03-04 16:54:48 +0900

    お久しぶりです
    回復処理をThread用の新しい内部クラスを用意して、プレイヤーとそのインスタンスをhealingPlayerに登録する方法にしたんだ～

--- src/main/java/jp/meao0525/battleemblem/beevent/RegainHealthEvent.java
+++ src/main/java/jp/meao0525/battleemblem/beevent/RegainHealthEvent.java
@@ -3,6 +3,7 @@ package jp.meao0525.battleemblem.beevent;
 import jp.meao0525.battleemblem.BattleEmblemMain;
 import jp.meao0525.battleemblem.begame.BeGame;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,25 +16,41 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RegainHealthEvent implements Listener {
 
     private Plugin plugin;
-    private static HashMap<Player,Integer> healingPlayer = new HashMap<Player, Integer>();
+    private static HashMap<Player,HealingThread> healingPlayer = new HashMap<>();
 
     public RegainHealthEvent(Plugin plugin) { this.plugin = plugin; }
 
     @EventHandler
-    public void PlayerStopEvent(PlayerToggleSneakEvent e) {
+    public void PlayerSneakEvent(PlayerToggleSneakEvent e) {
         //ゲーム中じゃない
         if (BeGame.getPhase() == 0) { return; }
 
-        //5秒止まると1ずつHPが回復
-        e.getPlayer().sendMessage("できてる?");
-        healingPlayer.put(e.getPlayer(),0);
-        countHealStart(e.getPlayer());
+        if (e.getPlayer().isSneaking()) {
+            //スニーク時
+            Player player = e.getPlayer();
+            player.sendMessage(ChatColor.YELLOW + "回復中...");
+
+            //新しいスレッド用インスタンス
+            HealingThread thread = new HealingThread(player);
+            //healingPlayerに登録
+            healingPlayer.put(player,thread);
+            return;
+
+        } else {
+            //スニーク解除時
+            if (healingPlayer.containsKey(e.getPlayer())) {
+                e.getPlayer().sendMessage(ChatColor.GRAY + "回復を中断しました");
+                healingPlayer.remove(e.getPlayer());
+            }
+            return;
+        }
     }
 
     @EventHandler
@@ -48,6 +65,7 @@ public class RegainHealthEvent implements Listener {
 
         //動いたらhealingリストから削除して回復できなくしてやる、フハハ
         if (healingPlayer.containsKey(e.getPlayer())) {
+            e.getPlayer().sendMessage(ChatColor.GRAY + "回復を中断しました");
             healingPlayer.remove(e.getPlayer());
         }
     }
@@ -60,24 +78,59 @@ public class RegainHealthEvent implements Listener {
         }
     }
 
-    public void countHealStart(Player player) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                //healingリストにいないか、HPが満タン
-                if ((!healingPlayer.containsKey(player) ) || (player.getHealth() == player.getHealthScale())) {
-                    cancel();
-                    return;
+    //Thread用の内部クラス
+    private class HealingThread {
+        private Player player;
+        private int count = 0;
+
+        private HealingThread(Player player) {
+            this.player = player;
+            countHealStart();
+        }
+
+        public void countHealStart() {
+            //5秒止まると1ずつHPが回復
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    //healingリストにいないか、HPが満タン
+                    if ((!healingPlayer.containsKey(player) ) || (player.getHealth() == player.getHealthScale())) {
+                        cancel();
+                        return;
+                    }
+                    //5秒以上経った
+                    if (count >= 5) {
+                        //HP1回復
+                        player.setHealth(player.getHealth() + 1.0);
+                        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP,5.0F,5.0F);
+                    } else {
+                        //経過秒数を1増やす
+                        count++;
+                        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP,5.0F,5.0F);
+                    }
                 }
-                //5秒以上経った
-                if (healingPlayer.get(player) >= 5) {
-                    //HP1回復
-                    player.setHealth(player.getHealth() + 1.0);
-                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP,5.0F,5.0F);
-                }
-                //経過秒数を1増やす
-                healingPlayer.put(player, healingPlayer.get(player) + 1);
-            }
-        }.runTaskTimer(plugin,0,20);
+            }.runTaskTimer(plugin,0,20);
+        }
     }
+
+//    public void countHealStart(Player player) {
+//        new BukkitRunnable() {
+//            @Override
+//            public void run() {
+//                //healingリストにいないか、HPが満タン
+//                if ((!healingPlayer.containsKey(player) ) || (player.getHealth() == player.getHealthScale())) {
+//                    cancel();
+//                    return;
+//                }
+//                //5秒以上経った
+//                if (healingPlayer.get(player) >= 5) {
+//                    //HP1回復
+//                    player.setHealth(player.getHealth() + 1.0);
+//                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP,5.0F,5.0F);
+//                }
+//                //経過秒数を1増やす
+//                healingPlayer.put(player, healingPlayer.get(player) + 1);
+//            }
+//        }.runTaskTimer(plugin,0,20);
+//    }
 }
