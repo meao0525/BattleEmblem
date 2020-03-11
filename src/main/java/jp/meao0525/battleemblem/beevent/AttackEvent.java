2b676fcf74e3ea0ce6bb02bc298d5154f02ad3b7
meao0525 <50980996+meao0525@users.noreply.github.com>
2020-03-11 23:07:34 +0900

    プレイヤーのデス処理
    AttackEventの中でプレイヤーのデス処理をした
    その他のダメージの時もやらないといけない

--- src/main/java/jp/meao0525/battleemblem/beevent/AttackEvent.java
+++ src/main/java/jp/meao0525/battleemblem/beevent/AttackEvent.java
@@ -6,7 +6,11 @@ import jp.meao0525.battleemblem.begame.BeGame;
 import jp.meao0525.battleemblem.beitem.BeItemName;
 import jp.meao0525.battleemblem.beplayer.BePlayer;
 import jp.meao0525.battleemblem.beplayer.BePlayerList;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.AbstractArrow;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
@@ -19,6 +23,7 @@ import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import static jp.meao0525.battleemblem.begame.BeLocation.coliseum;
 import static jp.meao0525.battleemblem.beitem.BeItemName.*;
 
 public class AttackEvent implements Listener {
@@ -37,7 +42,7 @@ public class AttackEvent implements Listener {
         if (!(e.getDamager() instanceof Player) || !(e.getEntity() instanceof Player)) {
             return;
         }
-
+        //各プレイヤーの取得
         Player attacker = (Player) e.getDamager();
         Player defender = (Player) e.getEntity();
         BePlayer beAttacker = BePlayerList.getBePlayer(attacker);
@@ -47,46 +52,30 @@ public class AttackEvent implements Listener {
         if ((beAttacker == null) || (beDefender == null)) {
             return;
         }
-
+        //所持アイテム取得
         ItemStack item = attacker.getInventory().getItemInMainHand();
         //素手で殴ってやがるよ笑
         if (item == null) {
             e.setCancelled(true);
             return;
         }
-
+        //アイテム名取得
         String itemName = item.getItemMeta().getDisplayName();
-        double attack = beAttacker.getAttack();
-        double defence = beDefender.getDefence();
-        switch (itemName) {
-            case MASTER_SWORD_NAME:
-            case KNIGHT_AXE_NAME:
-                e.setDamage(attack - defence);
-                return;
-            case BERSERKER_AXE_NAME:
-            case SNIPER_BOW_NAME:
-                e.setDamage(attack - defence);
-                KnockBack(attacker,defender);
-                return;
-            case BRAVE_SWORD_NAME:
-                //HP減少分の追加ダメージ
-                double decrement = ClassStatus.BRAVE_HERO_STATUS.getHp() - attacker.getHealth();
-                e.setDamage(attack - defence + decrement);
-                return;
-            case ASSASSIN_DAGGER_NAME:
-                //背後からの攻撃か?
-                if (isBackAttack(attacker,defender)) {
-                    //背後からだと防御無視+追加ダメージ
-                    attacker.sendMessage("背中に命中！");
-                    e.setDamage(attack + 6);
-                } else {
-                    e.setDamage(attack - defence);
-                }
-                return;
-            default:
-                e.setCancelled(true);
+        //ダメージ計算
+        double totalDamage = calcDamage(beAttacker,beDefender,itemName);
+        //ダメージ設定
+        e.setDamage(totalDamage);
+
+        //ノックバック
+        if ((itemName.equalsIgnoreCase(BERSERKER_AXE_NAME)) || (itemName.equalsIgnoreCase(SNIPER_BOW_NAME))) {
+            knockback(attacker,defender);
         }
 
+        //ダメージが残りHP以上であればデス処理
+        if (totalDamage >= defender.getHealth()) {
+            e.setCancelled(true);
+            PlayerDeath(beDefender);
+        }
     }
 
     @EventHandler
@@ -120,17 +109,88 @@ public class AttackEvent implements Listener {
         shooter.getInventory().addItem(new ItemStack(Material.ARROW));
     }
 
-    private boolean isBackAttack(Player attacker, Player defender) {
+    public double calcDamage(BePlayer beAttacker, BePlayer beDefender, String itemName) {
+        //攻撃力、防御力の取得
+        double attack = beAttacker.getAttack();
+        double defence = beDefender.getDefence();
+        double damage = 0;
+
+        //所持アイテムの名前で判定
+        switch (itemName) {
+            case MASTER_SWORD_NAME:
+            case KNIGHT_AXE_NAME:
+            case BERSERKER_AXE_NAME:
+            case SNIPER_BOW_NAME:
+                //通常ダメージ
+                damage = attack - defence;
+                break;
+
+            case BRAVE_SWORD_NAME:
+                //HP減少分の追加ダメージ
+                double decrement = ClassStatus.BRAVE_HERO_STATUS.getHp() - beAttacker.getPlayer().getHealth();
+                damage = attack - defence + decrement;
+                break;
+
+            case ASSASSIN_DAGGER_NAME:
+                //背後からの攻撃か?
+                if (isBackAttack(beAttacker,beDefender)) {
+                    //背後からだと防御無視+追加ダメージ
+                    damage = attack + 6;
+                } else {
+                    //通常ダメージ
+                    damage = attack - defence;
+                }
+                break;
+        }
+        return damage;
+    }
+
+    public void PlayerDeath(BePlayer bePlayer) {
+        //プレイヤーの取得
+        Player p = bePlayer.getPlayer();
+        //残機はなんぼ?
+        if (bePlayer.getLife() > 0) {
+            //ライフを1減らす
+            bePlayer.setLife(bePlayer.getLife() - 1);
+            //初期位置にTP
+            p.teleport(coliseum);
+            //HPを回復
+            p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
+            //残機を教えてあげて
+            p.sendMessage(ChatColor.GOLD + "[BattleEmblem]"
+                    + ChatColor.RESET + "残機は"
+                    + ChatColor.AQUA + bePlayer.getLife()
+                    + ChatColor.RESET + "です");
+        } else {
+            //バトルクラスを外す
+            bePlayer.removeBattleClass();
+            //プレイヤーリストから外す
+            BePlayerList.getBePlayerList().remove(bePlayer);
+            //デスメッセージ
+            Bukkit.broadcastMessage(ChatColor.GOLD + "[BattleEmblem]" + ChatColor.RESET + p.getPlayerListName() + " が脱落しました");
+
+            //残り人数が一人以下ならゲーム終了
+            if (BePlayerList.getBePlayerList().size() <= 1) {
+                BeGame.End();
+            }
+        }
+    }
+
+    private boolean isBackAttack(BePlayer beAttacker, BePlayer beDefender) {
+        /*
+         * プレイヤーの視点先のブロック表面の法線ベクトルを取得
+         * これを比較して一致すればとりあえず背後からの攻撃とする(これでは厳密な背後判定ではない)
+         */
+        Vector attackerDirection = beAttacker.getPlayer().getFacing().getDirection();
+        Vector defenderDirection = beDefender.getPlayer().getFacing().getDirection();
         //攻撃は背後からの攻撃か?
-        Vector attackerDirection = attacker.getFacing().getDirection();
-        Vector defenderDirection = defender.getFacing().getDirection();
         if (attackerDirection.equals(defenderDirection)) {
             return true;
         }
         return false;
     }
 
-    private void KnockBack(Player attacker, Player defender) {
+    private void knockback(Player attacker, Player defender) {
         //プレイヤーをノックバックさせる
         defender.setVelocity(attacker.getLocation().getDirection().setY(0).normalize().multiply(5));
     }
