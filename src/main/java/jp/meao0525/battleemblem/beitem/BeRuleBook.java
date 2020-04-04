35ebc71176b401eac7ae6d7ae585e939cc9c5e83
meao0525 <50980996+meao0525@users.noreply.github.com>
2020-04-04 22:06:10 +0900

    BeRuleBookを作成
    バトルクラスの説明を本で説明してやろう

---
+++ src/main/java/jp/meao0525/battleemblem/beitem/BeRuleBook.java
@@ -0,0 +1,13 @@
+package jp.meao0525.battleemblem.beitem;
+
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.meta.BookMeta;
+
+public class BeRuleBook {
+
+    private BookMeta meta;
+
+    private final String PAGE1_TITLE = ChatColor.GOLD + "===== Battle Emblem！ =====\n";
+    private final String PAGE1_DATA = "ロードアウトセレクターを右クリックして、バトルクラスを選択しよう\n（それぞれのバトルクラスの能力は次ページ以降）";
+
+}
